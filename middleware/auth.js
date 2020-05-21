2524c5af1c26d13f1394061d6ad2a633763d92f3
Andres <andres-a23@live.com>
2020-05-21 17:58:27 -0400

    setting up components and connecting back end to front end

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,8 +13,9 @@ module.exports = function (req, res, next) {
   // Verify token
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
-
+    console.log(decoded)
     req.user = decoded.user;
+    console.log(req.user)
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid'})
